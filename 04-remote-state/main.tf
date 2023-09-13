07f38f577be7afb8501d2b13af01d3f46d96b5ef
CodingManoj <mvp2612@hotmail.com>
2023-09-12 23:32:25 -0400

    Git Auto Push

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   count         = var.howManyYouWant
 
   ami           = "ami-0e977b99c38a13dfc"
-  instance_type = "t3.medium"
+  instance_type = "t3.mediumddfgdf"
 
   tags = {
     Name = "Terraform-Web-Instance-${count.index+1}"
