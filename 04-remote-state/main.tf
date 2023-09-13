6cbf64967c746ff1ab78f5a170d66d197a7c0cad
CodingManoj <mvp2612@hotmail.com>
2023-09-12 23:25:47 -0400

    Git Auto Push

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  count         = 0
+  count         = var.howManyYouWant
 
   ami           = "ami-0e977b99c38a13dfc"
   instance_type = "t3.medium"
--- 04-remote-state/provider.tf
+++ 04-remote-state/provider.tf
@@ -7,3 +7,7 @@ terraform {
     region = "us-east-1"
   }
 }
+
+variable "howManyYouWant" {
+    default = 2
+}
