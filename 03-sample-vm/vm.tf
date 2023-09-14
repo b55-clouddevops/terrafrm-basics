45f2130e5a5832a77722e35808efc2602c32a262
CodingManoj <mvp2612@hotmail.com>
2023-09-13 22:38:38 -0400

    Git Auto Push

--- 03-sample-vm/vm.tf
+++ 03-sample-vm/vm.tf
@@ -1,8 +1,12 @@
-resource "aws_instance" "app" {
+resource "aws_instance" "aws_instance" {
   ami           = "ami-0e977b99c38a13dfc"
   instance_type = "t3.medium"
 
   tags = {
     Name = "Terraform-Instance"
   }
+}
+
+output "private_ip_address" {
+  value = aws_instance.app.private_dns
 }
