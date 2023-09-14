f24216db4d5e9564d1b0b68f31cbcdfcece9370e
CodingManoj <mvp2612@hotmail.com>
2023-09-13 23:12:02 -0400

    Git Auto Push

--- 05-modules/backend.tf
+++ 05-modules/backend.tf
@@ -3,7 +3,7 @@ provider "aws" {}
 terraform {
   backend "s3" {
     bucket = "b55-terraform-state"
-    key    = "mod/terraform.tfstate"
+    key    = "tfmodules/terraform.tfstate"
     region = "us-east-1"
   }
 }
