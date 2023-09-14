c16aa405d25194dc6a35d518b621df6f70a96542
CodingManoj <mvp2612@hotmail.com>
2023-09-13 23:23:21 -0400

    Git Auto Push

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -2,9 +2,11 @@
 resource "aws_instance" "app" {
   ami                       = "ami-0e977b99c38a13dfc"
   instance_type             = "t3.medium"
-  vpc_security_group_ids    = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids    = [var.sg]
 
   tags = {
     Name = "Terraform-Instance"
   }
 }
+
+variable "sg"
--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -1,6 +1,7 @@
 # calling ec2Module
 module "ec2" {
     source      = "./ec2"
+    sg          = module.sg.sgid                     # Step 2 : Passing the info the module that needs this variable over the root module
 }
 
 # calling sgModule
