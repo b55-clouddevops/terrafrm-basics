983376bc13b506e803bb96c8c6e07133e2b3b299
CodingManoj <mvp2612@hotmail.com>
2023-09-13 22:26:06 -0400

    Git Auto Push

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -6,11 +6,15 @@ data "aws_ami" "ami" {
 }
 
 resource "aws_instance" "web" {
-  count             = var.howManyYouWant
+  count             = length(var.instances)
   ami               = data.aws_ami.ami.image_id
   instance_type     = "t3.medium"
 
   tags = {
-    Name            = "Terraform-Web-Instance-${count.index+1}"
+    Name            = "var.instances[count.index]"
   }
+}
+
+variable "instances" {
+  detault   = ["catalogue" , "user" , "cart"]
 }
