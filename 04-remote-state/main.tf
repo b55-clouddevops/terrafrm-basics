69c30bf57c538c33a06de331816e197e8fb8bb81
CodingManoj <mvp2612@hotmail.com>
2023-09-13 22:27:53 -0400

    Git Auto Push

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "web" {
   instance_type     = "t3.medium"
 
   tags = {
-    Name            = "var.instances[count.index]"
+    Name            = "${var.instances[count.index]}"
   }
 }
 
