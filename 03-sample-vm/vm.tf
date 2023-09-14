193f547908f2cf923f46b0c4dacfd27f0dc48b00
CodingManoj <mvp2612@hotmail.com>
2023-09-13 22:55:33 -0400

    Git Auto Push

--- 03-sample-vm/vm.tf
+++ 03-sample-vm/vm.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "app" {
-  ami           = "ami-0e977b99c38a13dfc"
-  instance_type = "t3.medium"
+  ami                       = "ami-0e977b99c38a13dfc"
+  instance_type             = "t3.medium"
+  vpc_security_group_ids    = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "Terraform-Instance"
