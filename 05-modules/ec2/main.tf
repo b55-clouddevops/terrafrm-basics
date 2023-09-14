6c45901b91b75ce2034e7128cd67ec9ca73e1dbe
CodingManoj <mvp2612@hotmail.com>
2023-09-13 23:23:42 -0400

    Git Auto Push

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -1,5 +1,5 @@
 # Creates EC2
-resource "aws_instance" "app" {
+resource "aws_instance" "moapp" {
   ami                       = "ami-0e977b99c38a13dfc"
   instance_type             = "t3.medium"
   vpc_security_group_ids    = [var.sg]
